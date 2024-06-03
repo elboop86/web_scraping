5f341a7a922fd4e7eb974be1e02a16ed880430ff
Javier Medina <javi86medina@hotmail.com>
2024-06-03 19:11:08 +0200

    Init commit web scraping

---
+++ from bs4 import BeautifulSoup.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+import requests
+
+# versiones
+import bs4 # solo para el chequeo
+print("Version de BeatifulSoup:", bs4.__version__)
+print(" Version para el requests:", requests.__version__)
+
+# Para Arrancar he de seleccionar todo y ejecutar en una ventana interactiva
